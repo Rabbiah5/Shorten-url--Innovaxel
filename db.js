7dad6ae40a9679b8873a731a5d22e9123337b950
rabbiah <rabbiahqamar5@gmail.com>
2025-07-16 19:14:38 +0500

    reverting and limiting main to readme file only

--- db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('url_shortener', 'root', 'rabbiah5qamar', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-module.exports = sequelize;
